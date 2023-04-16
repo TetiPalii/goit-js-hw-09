f64467ea770e95dff17b6c8ea8098affdcd8b07c
TetiPalii <TetianaPalii9491@gmail.com>
2023-04-16 16:51:32 +0200

    task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,47 @@
+const stepInput = document.querySelector('[name="step"]');
+const form = document.querySelector('.form');
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const timerId = setTimeout(() => {
+      if (shouldResolve) {
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      } else {
+        reject(`❌ Rejected promise ${position} in ${delay}ms`);
+      }
+    }, delay);
+  });
 }
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit() {
+  createPromise(2, 1500)
+    .then(({ position, delay }) => {
+      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
+}
+
+// const fetchUserFromServer = username => {
+//   return new Promise((resolve, reject) => {
+//     console.log(`Fetching data for ${username}`);
+
+//     setTimeout(() => {
+//       // Change value of isSuccess variable to simulate request status
+//       const isSuccess = true;
+
+//       if (isSuccess) {
+//         resolve('success value');
+//       } else {
+//         reject('error');
+//       }
+//     }, 2000);
+//   });
+// };
+
+// fetchUserFromServer('dfgh')
+//   .then(user => console.log(user))
+//   .catch(error => console.error(error));
