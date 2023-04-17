a123d09d63dc99afaaaad741b395d6e0740ae073
TetiPalii <116648151+TetiPalii@users.noreply.github.com>
2023-04-17 15:31:35 +0000

    Deploying to gh-pages from @ TetiPalii/goit-js-hw-09@4931c135eaa4dc340a9e2e6fae33aa1cba1005c5 🚀

--- 03-promises.78498f95.js
+++
@@ -1,2 +0,0 @@
-function e(e,o){const t=Math.random()>.3;return new Promise(((n,i)=>{setTimeout((()=>{t?n({position:e,delay:o}):i({position:e,delay:o})}),o)}))}document.querySelector(".form").addEventListener("submit",(function(o){o.preventDefault();const{elements:{delay:t,step:n,amount:i}}=o.currentTarget;for(let o=1;o<i;o+=1)e(o,t).then((({position:e,delay:o})=>{console.log(`✅ Fulfilled promise ${e} in ${o}ms`)})).catch((({position:e,delay:o})=>{console.log(`❌ Rejected promise ${e} in ${o}ms`)}))}));
-//# sourceMappingURL=03-promises.78498f95.js.map
